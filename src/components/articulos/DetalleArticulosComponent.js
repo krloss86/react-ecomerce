f6588c21a2c47d605371534453019ae318db3ae4
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-03-06 00:42:04 -0300

    fix json and image path

--- src/components/articulos/DetalleArticulosComponent.js
+++ src/components/articulos/DetalleArticulosComponent.js
@@ -26,7 +26,7 @@ export default function DetallearticulosComponent() {
 				<div className="card mb-3">
 				  <div className="row align-items-center p-1">
 				    <div className="col-md-5">
-				      <img src="./assets/images/ceramica.jpg" className="img-fluid rounded-start" alt="ceramica"/>
+				      <img src="../../assets/images/ceramica.jpg" className="img-fluid rounded-start" alt="ceramica"/>
 				    </div>
 				    <div className="col-md-7">
 						<div className="card-body">

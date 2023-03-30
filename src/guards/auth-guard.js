aefbbe9638be8e3c179f8cdb5b9650c7bf84ce36
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2023-03-30 16:30:04 -0300

    add guard

--- src/guards/auth-guard.js
+++ src/guards/auth-guard.js
@@ -1,6 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
 
 export function AuthGuard (){
  
     const loginStore = useSelector(store => store.login);
+    return loginStore.user ? <Outlet/> : <Navigate replace to={'/'}></Navigate>
 }
+
+export default AuthGuard;
